5935caaddd1ef5e0ead7222a02767763738c2350
karthikeya-arikirevula-ics4u1-02-25 <karthikeya.arikirevula@student.tdsb.on.ca>
2025-04-03 13:47:30 +0000

    a

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,36 @@ public class PracticeProblem {
 
 	}
 
+	public static int find(int[] arr, int num) {
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == num) {
+				return i;
+			}
+		}
+		return -1;
+	}
 	
+	public static int findLast(String[] arr, String word) {
+		for (int i = arr.length - 1; i >= 0; i--) {
+			if (arr[i].equals(word)) {
+				return i;
+			}
+		}
+		return -1;
+	}
 
+	public static int findSecond(char[] arr, char letter) {
+		int counter = -1;
+		
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == letter) {
+				if (counter == -1) {
+					counter = i;
+				} else {
+					return i;
+				}
+			}
+		}
+		return counter;
+	}
 }
